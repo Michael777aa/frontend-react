24e3d7e71718d01e3d9d8af6b5d258775e192cd9
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-08-28 00:33:31 +0900

    feat: develop statistics sectional component

--- src/app/screens/homepage/Statistics.tsx
+++ src/app/screens/homepage/Statistics.tsx
@@ -1,36 +1,45 @@
 import React from "react";
 import { Box, Container, Stack } from "@mui/material";
-import Divider from "../../components/divider";
-
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import SecuritySharpIcon from "@mui/icons-material/SecuritySharp";
+import MultipleStopSharpIcon from "@mui/icons-material/MultipleStopSharp";
+import LocalPhoneSharpIcon from "@mui/icons-material/LocalPhoneSharp";
+import Typography from "../../MaterialTheme/typography";
 export default function Statistics() {
   return (
     <div className={"static-frame"}>
       <Container>
-        <Stack data-aos="fade-up" className="info">
+        <Stack className="info">
           <Stack className="static-box">
-            <Box className={"static-num"}>12</Box>
-            <Box className={"static-text"}>Restaurants</Box>
+            <Box className={"round"}>
+              <LocalShippingIcon className="icon" />
+            </Box>
+            <h2 className={"shipping"}>Free Shipping</h2>
+            <h3 className={"order"}>Free on order over $300</h3>
           </Stack>
 
-          <Divider height="64" width="2" bg="#E3C08D" />
-
           <Stack className="static-box">
-            <Box className={"static-num"}>8</Box>
-            <Box className={"static-text"}>Experience</Box>
+            <Box className={"round"}>
+              <SecuritySharpIcon className="icon" />
+            </Box>
+            <h2 className={"shipping"}>Security Payment</h2>
+            <h3 className={"order"}>100% security payment</h3>
           </Stack>
 
-          <Divider height="64" width="2" bg="#E3C08D" />
-
           <Stack className="static-box">
-            <Box className={"static-num"}>50+</Box>
-            <Box className={"static-text"}>Menu</Box>
+            <Box className={"round"}>
+              <MultipleStopSharpIcon className="icon" />
+            </Box>
+            <h2 className={"shipping"}>30 Day Return</h2>
+            <h3 className={"order"}>30 day money guarantee</h3>
           </Stack>
 
-          <Divider height="64" width="2" bg="#E3C08D" />
-
           <Stack className="static-box">
-            <Box className={"static-num"}>200+</Box>
-            <Box className={"static-text"}>Clients</Box>
+            <Box className={"round"}>
+              <LocalPhoneSharpIcon className="icon" />
+            </Box>
+            <h2 className={"shipping"}>24/7 Support</h2>
+            <h3 className={"order"}>Support every time fast</h3>
           </Stack>
         </Stack>
       </Container>
