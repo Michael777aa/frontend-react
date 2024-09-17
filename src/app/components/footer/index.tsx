a8f92b6fd23e2f95d103e2d2a57e2cd56c39f203
Michael777aa <shoxabdufattoyev485@gmail.com>
2024-09-18 00:44:17 +0900

    feat: modify many logics

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,87 +1,215 @@
-import React from "react";
-import { Box, Container, Stack } from "@mui/material";
-import { Link } from "react-router-dom";
+import {
+  Box,
+  Button,
+  Container,
+  Stack,
+  Typography,
+  TextField,
+} from "@mui/material";
 import styled from "styled-components";
+import SearchIcon from "@mui/icons-material/Search";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { Link } from "react-router-dom";
 
 const Footers = styled.div`
   width: 100%;
-  height: 590px;
-  display: flex;
-  background: #343434;
-  background-size: cover;
+  padding: 60px 0;
+  background-color: #747d88;
+  color: #fff;
 `;
+
+const FooterInput = styled(TextField)(({ theme }) => ({
+  "& .MuiOutlinedInput-root": {
+    borderRadius: "50px",
+    backgroundColor: "#fff",
+    width: "636px",
+    height: "59px",
+    "& fieldset": {
+      border: "none",
+    },
+  },
+}));
+
+const FooterButton = styled(Button)(({ theme }) => ({
+  backgroundColor: "#81c408",
+  color: "#fff",
+  borderRadius: "25px",
+  padding: "10px 25px",
+  fontWeight: 700,
+  "&:hover": {
+    backgroundColor: "#ffb514",
+  },
+}));
+const FooterButton2 = styled(Button)(({ theme }) => ({
+  backgroundColor: "#81c408",
+  color: "#fff",
+  borderRadius: "25px",
+  padding: "10px 15px",
+  fontWeight: 700,
+  "&:hover": {
+    backgroundColor: "#ffb514",
+  },
+}));
 
 export default function Footer() {
   const authMember = null;
-
   return (
     <Footers>
       <Container>
-        <Stack flexDirection={"row"} sx={{ mt: "94px" }}>
-          <Stack flexDirection={"column"} style={{ width: "340px" }}>
-            <Box>
-              <img width={"100px"} src={"/icons/burak.svg"} />
-            </Box>
-            <Box className={"foot-desc-txt"}>
-              Focusing on the gourmet Turkish breakfast as well as the youth
-              society, CZN Burak Gurme aims to bring Turkish cuisine back. CZN
-              Burak Gurme creates an illusion with its cuisine.
-            </Box>
-            <Box className="sns-context">
-              <img src={"/icons/facebook.svg"} />
-              <img src={"/icons/twitter.svg"} />
-              <img src={"/icons/instagram.svg"} />
-              <img src={"/icons/youtube.svg"} />
+        <Stack
+          className="main-upper-container"
+          direction={{ xs: "column", md: "row" }}
+          justifyContent="space-between"
+          alignItems="center"
+          spacing={4}
+        >
+          {/* Left Side */}
+          <Stack className="left-side-up">
+            <Typography variant="h4" className="left-entitle">
+              FreshNest
+            </Typography>
+            <Typography variant="subtitle1" className="left-topic">
+              Fresh Products
+            </Typography>
+          </Stack>
+
+          {/* Middle Side (Search Box) */}
+          <Stack className="middle-side-up" width={{ xs: "100%", md: "50%" }}>
+            <Box className="search-box-main" display="flex" alignItems="center">
+              <FooterInput placeholder="Enter your email" />
+              <Button className="search-icon">Subscribe </Button>
             </Box>
           </Stack>
-          <Stack sx={{ ml: "288px" }} flexDirection={"row"}>
-            <Stack>
-              <Box>
-                <Box className={"foot-category-title"}>Bo'limlar</Box>
-                <Box className={"foot-category-link"}>
-                  <Link to="/">Home</Link>
-                  <Link to="/products">Products</Link>
-                  {authMember && <Link to="/orders">Orders</Link>}
-                  <Link to="/help">Help</Link>
-                </Box>
-              </Box>
-            </Stack>
-            <Stack sx={{ ml: "100px" }}>
-              <Box>
-                <Box className={"foot-category-title"}>Find us</Box>
-                <Box
-                  flexDirection={"column"}
-                  sx={{ mt: "20px" }}
-                  className={"foot-category-link"}
-                  justifyContent={"space-between"}
-                >
-                  <Box flexDirection={"row"} className={"find-us"}>
-                    <span>L.</span>
-                    <div>Downtown, Dubai</div>
-                  </Box>
-                  <Box className={"find-us"}>
-                    <span>P.</span>
-                    <div>+971 4 554 7777</div>
-                  </Box>
-                  <Box className={"find-us"}>
-                    <span>E.</span>
-                    <div>devexuz@gmail.com</div>
-                  </Box>
-                  <Box className={"find-us"}>
-                    <span>H.</span>
-                    <div>Visit 24 hours</div>
-                  </Box>
-                </Box>
-              </Box>
-            </Stack>
+
+          {/* Right Side (Icons) */}
+          <Stack
+            className="right-side-up"
+            direction="row"
+            justifyContent="center"
+          >
+            <a
+              href="https://youtu.be/eDqm9H1-YiE?si=rtRoEuKm_ZqICOu2"
+              className="icon"
+            >
+              <TwitterIcon />
+            </a>
+
+            <a
+              href="https://youtu.be/ASqHa-hgzVY?si=XYVMacja98nkqMrg"
+              className="icon"
+            >
+              <FacebookIcon />
+            </a>
+
+            <a
+              href="https://youtu.be/Cmm-itIRPU8?si=by1EFWV2b04sbwyD"
+              className="icon"
+            >
+              <YouTubeIcon />
+            </a>
+
+            <a
+              href="https://youtu.be/ANsQzuBNqE8?si=9ReSJ_iraNXY7wPg"
+              className="icon"
+            >
+              <LinkedInIcon />
+            </a>
           </Stack>
         </Stack>
+
+        {/* Divider Line */}
+        <Box className="hover-linesss" borderBottom="1px solid #ffb514" />
+
+        {/* Main Content */}
         <Stack
-          style={{ border: "1px solid #C5C8C9", width: "100%", opacity: "0.2" }}
-          sx={{ mt: "80px" }}
-        ></Stack>
-        <Stack className={"copyright-txt"}>
-          © Copyright Devex Global, All rights reserved.
+          className="middle-main-container"
+          direction={{ xs: "column", md: "row" }}
+          justifyContent="space-between"
+          mt={4}
+          spacing={4}
+        >
+          {/* Section 1 */}
+          <Stack className="left-side1 same">
+            <Typography variant="h1">Why People Like Us!</Typography>
+            <Typography variant="h2" className="left-words">
+              Our seamless online ordering process, fast delivery, and
+              eco-friendly packaging make it convenient and environmentally
+              responsible to enjoy fresh produce.
+            </Typography>
+            <Button className="down-button">Read More</Button>
+          </Stack>
+
+          {/* Section 2 */}
+          <Stack className="left-side2 same color-hover">
+            <Link to="/help" className="ssss">
+              <Typography variant="h1">Shop Info</Typography>
+            </Link>
+            <Link to="/help" className="ssss">
+              <Typography variant="h2">About Us</Typography>
+            </Link>
+            <Link to="/help" className="ssss">
+              <Typography variant="h2">Contact Us</Typography>
+            </Link>
+            <Link to="/help" className="ssss">
+              <Typography variant="h2">Privacy Policy</Typography>
+            </Link>
+            <Link to="/help" className="ssss">
+              <Typography variant="h2">Terms & Condition</Typography>
+            </Link>
+            <Link to="/help" className="ssss">
+              <Typography variant="h2">Return Policy</Typography>
+            </Link>
+          </Stack>
+
+          <Stack className="right-side1 color-hover same">
+            <Link to="/member-page" className="ssss">
+              <Typography variant="h1">Account</Typography>
+            </Link>
+
+            <Link to="/member-page" className="ssss">
+              <Typography variant="h2">My Account</Typography>
+            </Link>
+
+            <Link to="/product-detail" className="ssss">
+              <Typography variant="h2">Product details</Typography>
+            </Link>
+
+            <Link to="/orders" className="ssss">
+              <Typography variant="h2">Shopping Cart</Typography>
+            </Link>
+
+            <Link to="/orders" className="ssss">
+              <Typography variant="h2">WishList</Typography>
+            </Link>
+
+            <Link to="/orders" className="ssss">
+              <Typography variant="h2">Order History</Typography>
+            </Link>
+          </Stack>
+
+          {/* Section 4 */}
+          <Stack className="right-side2 same">
+            <Link to="/help" className="ssss">
+              <Typography variant="h1">Contact</Typography>
+            </Link>
+            <Typography variant="h2">
+              Address: 1429 Netus Rd, NY 48247
+            </Typography>
+            <Typography variant="h2">
+              Email: abdullah.just777@gmail.com
+            </Typography>
+            <Typography variant="h2">Phone: +82 10 7555 1575</Typography>
+            <Typography variant="h2">Payment Accepted</Typography>
+            <Box display="flex" gap={2} mt={4}>
+              <img src="/icons/visa-card.svg" alt="Visa" />
+              <img src="/icons/western-card.svg" alt="Western Union" />
+              <img src="/icons/master-card.svg" alt="MasterCard" />
+              <img src="/icons/paypal-card.svg" alt="PayPal" />
+            </Box>
+          </Stack>
         </Stack>
       </Container>
     </Footers>
